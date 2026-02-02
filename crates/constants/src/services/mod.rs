4d9ffad57e060196d259fef5c76b3ead920d9667
RosieTheGhostie <rosetheghost.dev@gmail.com>
2026-02-01 19:47:32 -0700

    fix(constants): `Services` deserialization relying on string-based service codes

--- crates/constants/src/services/mod.rs
+++ crates/constants/src/services/mod.rs
@@ -4,6 +4,9 @@ pub mod spim;
 
 pub use service::Service;
 
+#[cfg(feature = "serde")]
+mod deserialize;
+
 use std::collections::{HashMap, hash_map};
 
 use seaside_type_aliases::ServiceCode;
@@ -50,44 +53,6 @@ impl Services {
 
     pub fn iter(&self) -> hash_map::Iter<'_, ServiceCode, Service> {
         self.data.iter()
-    }
-}
-
-#[cfg(feature = "serde")]
-impl<'de> serde::Deserialize<'de> for Services {
-    fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
-    where
-        D: serde::Deserializer<'de>,
-    {
-        use core::fmt::{self, Formatter};
-
-        use seaside_type_aliases::ServiceCode;
-        use serde::de::{Error, MapAccess, Visitor};
-
-        pub struct ServicesVisitor;
-
-        impl<'de> Visitor<'de> for ServicesVisitor {
-            type Value = Services;
-
-            fn expecting(&self, formatter: &mut Formatter) -> fmt::Result {
-                formatter.write_str("a mapping from service codes to services")
-            }
-
-            fn visit_map<A>(self, mut access: A) -> Result<Self::Value, A::Error>
-            where
-                A: MapAccess<'de>,
-            {
-                let mut services = Services::with_capacity(access.size_hint().unwrap_or(0));
-                while let Some((code, service)) = access.next_entry::<String, Service>()? {
-                    let code: ServiceCode = code.parse().map_err(Error::custom)?;
-                    services.insert(code, service);
-                }
-
-                Ok(services)
-            }
-        }
-
-        deserializer.deserialize_map(ServicesVisitor)
     }
 }
 
